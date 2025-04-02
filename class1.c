a0d1916d221aeb80ec7d6335624747a8c0298a3b
stravo1@mac <stravoone@gmail.com>
2025-04-02 21:42:12 +0530

    added lots of funs

--- class1.c
+++ class1.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+int fun(){
+    return 0;
+}
+
+int fun1(){
+    return 0;
+}
+
+int fun2(){
+    return 0;
+}
+
+int fun3(){
+    return 0;
+}
+
+int fun4(){
+    return 0;
+}
+
+
 int main(){
     printf("Hi!");
 }
